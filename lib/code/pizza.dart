49351a19384dc1a04ee954773c5207d65e5eca40
Mostefa Mourad <47932282+MostefaMourad@users.noreply.github.com>
2019-10-27 22:23:02 +0100

    Create pizza.dart

---
+++ lib/code/pizza.dart
@@ -0,0 +1,23 @@
+class piza{
+
+static List<String> sizes = <String>["small","medium","large"];
+String size = sizes.elementAt(0);
+Map<String,bool> toppings = new Map<String,bool>();
+
+pizza(){
+  toppings.putIfAbsent('Pepperoni', ()=> false);
+  toppings.putIfAbsent('Sausage', ()=> false);
+  toppings.putIfAbsent('Cheese', ()=> false);
+  toppings.putIfAbsent('Pineapple', ()=> false);
+  toppings.putIfAbsent('Sardine', ()=> false);
+  toppings.putIfAbsent('Onions', ()=> false);
+  toppings.putIfAbsent('Mushrooms', ()=> false);
+  toppings.putIfAbsent('Peppers', ()=> false);
+  toppings.putIfAbsent('Pickles', ()=> false);
+  toppings.putIfAbsent('Choclate', ()=> false);
+  toppings.putIfAbsent('Banana', ()=> false);
+  toppings.putIfAbsent('Olives', ()=> false);
+}
+
+
+}
